cdc7c936a639b94fb7ad9b3e55303712edf1ab70
Aleksei <kilroygood@gmail.com>
2022-05-20 15:16:38 +0300

    addHardhatAndStartCoding

---
+++ contracts/Greeter.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract MyFarm{
+    address owner = msg.sender;
+
+    struct MyPet {
+        string name;
+        uint Enimaltype;
+        uint level;
+        uint hungry;
+        uint profit;
+    }
+
+    MyPet[] public myPets;
+
+    // event addPet(string name, uint Enimaltype, uint level, uint hungry, uint profit);
+
+    constructor() {
+        owner = msg.sender;
+    }
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Not an owner");
+        _;
+    }
+
+    uint randNonce = 0;
+    function randMod10() internal returns(uint){
+        randNonce++; 
+        return uint(keccak256(abi.encodePacked(msg.sender,randNonce))) % 10;
+    }
+    function randMod100() internal returns(uint){
+        randNonce++; 
+        return uint(keccak256(abi.encodePacked(msg.sender,randNonce))) % 100;
+    }
+
+    function createPrt(string memory _name, uint _Enimaltype) external onlyOwner{
+
+        MyPet memory newPet = MyPet({
+            name: _name,
+            Enimaltype: _Enimaltype,
+            level: randMod10(),
+            hungry: randMod100(),
+            profit: randMod100()
+        });
+        // emit addPet(_name, _Enimaltype, randMod10(), randMod100(), randMod100());
+
+        myPets.push(newPet);
+    }
+
+}
